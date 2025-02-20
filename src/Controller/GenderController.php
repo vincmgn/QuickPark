89820aa4f30e4a8b6e09f58405f74d8e7af87af9
vincmgn <vincentmagnienpro@gmail.com>
2025-02-20 15:02:33 +0100

    [feat] implement cache and validators for gender

--- src/Controller/GenderController.php
+++ src/Controller/GenderController.php
@@ -14,6 +14,9 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use OpenApi\Attributes as OA;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[Route('/api/gender')]
 #[OA\Tag(name: 'Gender')]
@@ -23,16 +26,26 @@ final class GenderController extends AbstractController
 {
     #[Route('s', name: 'app_gender', methods: ['GET'])]
     #[OA\Response(response: 200, description: 'Success', content: new Model(type: Gender::class))]
-    public function index(GenderRepository $genderRepository, SerializerInterface $serializerInterface): JsonResponse
+    /**
+     * Get all genders
+     */
+    public function index(GenderRepository $genderRepository, SerializerInterface $serializerInterface, TagAwareCacheInterface $cache): JsonResponse
     {
-        $gender = $genderRepository->findAll();
-        $jsonGender = $serializerInterface->serialize($gender, 'json');
+        $idCache = "getAllGender";
+        $jsonGender = $cache->get($idCache, function (ItemInterface $item) use ($genderRepository, $serializerInterface) {
+            $item->tag("Gender");
+            $gender = $genderRepository->findAll();
+            return $serializerInterface->serialize($gender, 'json');
+        });
 
         return new JsonResponse($jsonGender, JsonResponse::HTTP_OK, [], true);
     }
 
     #[Route('/{id}', name: 'gender_get', methods: ['GET'])]
     #[OA\Response(response: 200, description: 'Success', content: new Model(type: Gender::class))]
+    /**
+     * Get a specific gender by ID
+     */
     public function get(Gender $gender, SerializerInterface $serializerInterface): JsonResponse
     {
         $jsonGender = $serializerInterface->serialize($gender, 'json');
@@ -40,13 +53,22 @@ final class GenderController extends AbstractController
         return new JsonResponse($jsonGender, JsonResponse::HTTP_OK, [], true);
     }
 
-    #[Route('/{id}', name: 'gender_add', methods: ['POST'])]
+    #[Route('', name: 'gender_add', methods: ['POST'])]
     #[OA\Response(response: 201, description: 'Created', content: new Model(type: Gender::class))]
-    public function add(Request $request, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, UrlGeneratorInterface $urlGenerator): JsonResponse
+    #[OA\RequestBody(required: true, content: new OA\JsonContent(example: ["name" => "example"]))]
+    /**
+     * Add a new gender
+     */
+    public function add(Request $request, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, UrlGeneratorInterface $urlGenerator, TagAwareCacheInterface $cache, ValidatorInterface $validator): JsonResponse
     {
         $gender = $serializerInterface->deserialize($request->getContent(), Gender::class, 'json');
+        $errors = $validator->validate($gender);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializerInterface->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
         $entityManagerInterface->persist($gender);
         $entityManagerInterface->flush();
+        $cache->invalidateTags(["Gender"]);
 
         $jsonGender = $serializerInterface->serialize($gender, 'json');
         $location = $urlGenerator->generate("gender_get", ['id' => $gender->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
@@ -56,20 +78,30 @@ final class GenderController extends AbstractController
 
     #[Route('/{id}', name: 'gender_update', methods: ['PUT'])]
     #[OA\Response(response: 204, description: 'No content')]
-    public function update(Request $request, Gender $gender, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface): JsonResponse
+    #[OA\RequestBody(required: true, content: new OA\JsonContent(example: ["name" => "example"]))]
+    /**
+     * Update an existing gender by ID
+     *
+     */
+    public function update(Gender $gender, Request $request, SerializerInterface $serializerInterface, EntityManagerInterface $entityManagerInterface, TagAwareCacheInterface $cache): JsonResponse
     {
         $gender = $serializerInterface->deserialize($request->getContent(), Gender::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $gender]);
         $entityManagerInterface->flush();
+        $cache->invalidateTags(["Gender"]);
 
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, [], false);
     }
 
     #[Route('/{id}', name: 'gender_delete', methods: ['DELETE'])]
     #[OA\Response(response: 204, description: 'No content')]
-    public function delete(Gender $gender, EntityManagerInterface $entityManagerInterface): JsonResponse
+    /**
+     * Delete a specific gender by ID
+     */
+    public function delete(Gender $gender, EntityManagerInterface $entityManagerInterface, TagAwareCacheInterface $cache): JsonResponse
     {
         $entityManagerInterface->remove($gender);
         $entityManagerInterface->flush();
+        $cache->invalidateTags(["Gender"]);
 
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, [], false);
     }
--- src/Entity/Gender.php
+++ src/Entity/Gender.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\GenderRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GenderRepository::class)]
@@ -16,6 +17,14 @@ class Gender
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\NotNull]
+    #[Assert\Length(
+        min: 3,
+        max: 25,
+        minMessage: 'The gender name must be at least {{ limit }} characters long',
+        maxMessage: 'The gender name cannot be longer than {{ limit }} characters'
+    )]
     private ?string $name = null;
 
     public function getId(): ?int
