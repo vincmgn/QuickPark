dd02adf57c335e430f44b338844bb59b41073d95
vincmgn <vincentmagnienpro@gmail.com>
2025-03-25 23:29:58 +0100

    [feat] improve normalizer

--- src/Serializer/Normalizer/AutoDiscoveryNormalizer.php
+++ src/Serializer/Normalizer/AutoDiscoveryNormalizer.php
@@ -13,6 +13,7 @@ use App\Entity\Price;
 use App\Entity\User;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class AutoDiscoveryNormalizer implements NormalizerInterface
@@ -20,7 +21,8 @@ class AutoDiscoveryNormalizer implements NormalizerInterface
     public function __construct(
         #[Autowire(service: 'serializer.normalizer.object')]
         private NormalizerInterface $normalizer,
-        private UrlGeneratorInterface $urlGenerator
+        private UrlGeneratorInterface $urlGenerator,
+        private RouterInterface $router
     ) {}
 
     public function normalize($object, ?string $format = null, array $context = []): array
@@ -28,32 +30,32 @@ class AutoDiscoveryNormalizer implements NormalizerInterface
         $data = $this->normalizer->normalize($object, $format, $context);
         $className = (new \ReflectionClass($object))->getShortName();
         $className = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $className));
-        $data["_links"] = [
-            "getAll" => [
-                "method" => "GET",
-                "path" => $this->urlGenerator->generate("api_" . $className . "_getAll")
 
-            ],
-            "self" => [
-                "method" => "GET",
-                "path" => $this->urlGenerator->generate("api_" . $className . "_get", ["id" => $data["id"]])
-            ],
-            "new" => [
-                "method" => "POST",
-                "path" => $this->urlGenerator->generate("api_" . $className . "_new")
-            ],
-            "edit" => [
-                "method" => "PUT",
-                "path" => $this->urlGenerator->generate("api_" . $className . "_edit", ["id" => $data["id"]])
-            ],
-            "delete" => [
-                "method" => "DELETE",
-                "path" => $this->urlGenerator->generate("api_" . $className . "_delete", ["id" => $data["id"]])
-            ]
+        $availableRoutes = $this->router->getRouteCollection()->all();
+
+        $routes = [
+            "getAll" => ["method" => "GET", "route" => "api_{$className}_getAll"],
+            "self" => ["method" => "GET", "route" => "api_{$className}_get", "params" => ["id" => $data["id"]]],
+            "new" => ["method" => "POST", "route" => "api_{$className}_new"],
+            "edit" => ["method" => "PUT", "route" => "api_{$className}_edit", "params" => ["id" => $data["id"]]],
+            "delete" => ["method" => "DELETE", "route" => "api_{$className}_delete", "params" => ["id" => $data["id"]]],
         ];
+
+        $data["_links"] = [];
+
+        foreach ($routes as $key => $route) {
+            if (isset($availableRoutes[$route["route"]])) {
+                $params = $route["params"] ?? [];
+                $data["_links"][$key] = [
+                    "method" => $route["method"],
+                    "path" => $this->urlGenerator->generate($route["route"], $params)
+                ];
+            }
+        }
 
         return $data;
     }
+
 
     public function supportsNormalization($data, ?string $format = null, array $context = []): bool
     {
